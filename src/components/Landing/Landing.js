a6340214e26a11ecccd00e1db80d82b9b9cd221d
Patrick Thibodeau <patrick.thibodeau@gmail.com>
2019-12-06 15:52:12 -0600

    created landing page, header, login, register + some routes

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./Landing.css";
+
+export default class Landing extends Component {
+  render() {
+    return <div className="Landing-container"></div>;
+  }
+}
