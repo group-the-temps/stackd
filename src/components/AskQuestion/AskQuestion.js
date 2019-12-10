80d027caae61ac461b899ea58ab06885fd61086e
Patrick Thibodeau <patrick.thibodeau@gmail.com>
2019-12-10 11:12:34 -0600

    adjusted landing page background and added AskQuestion component

---
+++ src/components/AskQuestion/AskQuestion.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./AskQuestion.css";
+
+export default class AskQuestion extends Component {
+  render() {
+    return (
+      <>
+        <div></div>
+      </>
+    );
+  }
+}
