2877b207f4e7401b6b04ffabcba94fd328d1b972
James Blount IV <j.blount166@gmail.com>
2019-12-11 09:49:05 -0600

    added profileReducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import {createStore, combineReducers, applyMiddleware} from "redux";
 import promise from "redux-promise-middleware";
 import authReducer from "./authReducer";
+import profileReducer from "./profileReducer";
 
 const root = combineReducers({
-    authReducer
+    authReducer,
+    profileReducer
 })
 
 export default createStore(root, applyMiddleware(promise));
