920d02e6aa0ea9238dbaaa30a264c45a07486ef1
Ben Anderson <ben@Bens-MacBook-Pro.local>
2019-12-16 09:23:48 -0600

    toyed with backend

--- db/questions/answers/get_selected_answers.sql
+++ db/questions/answers/get_selected_answers.sql
@@ -1,2 +1,6 @@
 SELECT * FROM answers
-WHERE question_id = $1;
+WHERE questiond_id = $1;
+
+-- SELECT a.*, u.display_name, u.cohort FROM answers AS a
+-- INNER JOIN users AS u ON u.user_id = a.user_id
+-- WHERE a.question_id = $1 AND u.user_id = $2;
